567743226fe40a10ae3b3237dca7acb3b1109516
Philbranding <philbranding@gmail.com>
2020-05-11 09:24:40 +0200

    aufgabe1_5 definitions setup

---
+++ source/aufgabe1_5.cpp
@@ -0,0 +1,31 @@
+/* 
+
+DECLARATION
+The declaration means we are saying to a computer that this “thing” 
+(it can be a variable, a function or a class) exists but we don’t know where. 
+In the future, we may tell but right now it just exists somewhere. 
+In simple words, we don’t allocate memory while declaring. 
+We can declare that “thing” many times.
+
+DEFINATION
+The definition means we are saying to the computer that this “thing” needs memory 
+and it needs to be located somewhere. In simple words, defining means we have allocated memory for it. 
+We can define something only once
+
+INTIALIZATION
+The initialization means whatever our “thing “ is, we are giving it an initial value. 
+That “thing” must be in some memory location and if we keep that location empty, it may be a house for bugs and errors. 
+Initialization is not always necessary but it’s important.
+
+*/
+
+//Example
+
+//Variables
+
+int var; 	 // declaration of a variable called var
+var=10; 	 // initialize of the variable with a value of 10
+int var=10;  // definetion of a memory space, calling it var and assigning a value of 10*/
+
+//Functions
+static int add(int,int); //declaration of the funtion called add that 
