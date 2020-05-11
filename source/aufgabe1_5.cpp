c83da234a1b8548a50bc85bafabb9e030cdedb11
Philbranding <philbranding@gmail.com>
2020-05-11 12:15:21 +0200

    aufgabe1_5 definitions of classes and structs

--- source/aufgabe1_5.cpp
+++ source/aufgabe1_5.cpp
@@ -34,11 +34,92 @@ Initialization is not always necessary but it’s important.
 
 //Example
 
-//Variables
 
-extern int var; 	 // declaration of a variable called var
-var=10; 	 // initialize of the variable with a value of 10
-int var=10;  // definetion of a memory space, calling it var and assigning a value of 10*/
+
+//Variables
+#include <iostream>
+#include <string>
+using namespace std;
+
+extern int var; 	 // declaration of a variable called var and can be called by just using var = 10 for example
+int var = 10; 	 // initialize of the variable with a value of 10
+int var2 = 20;  // definetion of a memory space, calling it var2 and assigning a value of 10*/
+
+
+
+
 
 //Functions
-static int add(int,int); //declaration of the funtion called add that 
+extern int add(int,int); //declaration of the function called add that takes two integers and returns another int (signature)
+
+int add(int a, int b); //
+
+static int multiply(int a=10, int b=20){ //Function Intialization 
+	int result = a * b;
+	return result;
+}
+
+static double add(double a, double b){ // Function Definition 
+	return a + b; 
+}
+
+
+
+
+
+//Class
+
+//Declaration
+class klass;
+struct ztruct;
+
+
+//Definition 
+class MyClass {
+public:
+	void getprint(string mgs){
+		string message = mgs;
+	}
+	
+private:
+	void print( string message){
+
+		cout << message <<endl;
+	}
+};
+
+struct MyStruct {
+	void print(){
+		cout << "this is a STRUCT that has been declared and defined" <<endl;
+	}
+};
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	//cout << add(2,3) << endl;
+	cout << "Variables" <<endl;
+	cout << "extern Variable results 					:" << var << endl;
+	cout << "Variable declaration and definition        :" << var2 << endl;
+
+	cout << " " <<endl;
+	cout << "Functions" <<endl;
+	cout << "Function Definition results                : " << add(2.3, 5.0) << endl;
+	cout << "Function Asignment results                 : " << multiply() <<endl;
+	cout << "Function Declaration and Definition results: " << multiply(2,2) << endl;
+
+	cout << " " <<endl;
+	cout << "Class" << endl;
+
+	MyStruct A;
+	A.print();
+
+	MyClass B;
+	B.getprint("this is a CLASS that has been declared and defined");
+
+
+	return 0;
+}
