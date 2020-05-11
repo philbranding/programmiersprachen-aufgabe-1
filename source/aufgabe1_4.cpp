f7772e0819aa798631a32cb0c21dce0de4031634
Philbranding <philbranding@gmail.com>
2020-05-11 08:45:39 +0200

    setup of aufgabe1_4

---
+++ source/aufgabe1_4.cpp
@@ -0,0 +1,23 @@
+/* Aufgabe 1.4
+Informieren Sie sich über Typen in C++. 
+Benennen Sie im Folgenden Beispiel Typen, Variablen und Werte. 
+Erklären Sie in diesem Zusammenhang das Schlüs- selwort const. 
+Was ist eine Typkonvertierung und wieso kann es dabei Probleme
+geben? Wo sehen Sie Probleme im nachfolgenden Programmcode?
+
+int main() {
+	int 		a 	= 9;
+	bool 		b 	= false;
+	char 		c 	= ’a’;
+	double 		d 	= 1.3;
+	int const five 	= 5;
+	double 		e 	= a/five;
+
+	five = d; 
+
+	return 0;
+}
+
+*/
+
+
