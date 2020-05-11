a7e505983188fa2577e792d2d789dfe6fe74625f
Philbranding <philbranding@gmail.com>
2020-05-11 09:03:35 +0200

    aufgabe1_4 answered all part

--- source/aufgabe1_4.cpp
+++ source/aufgabe1_4.cpp
@@ -1,4 +1,5 @@
 /* Aufgabe 1.4
+
 Informieren Sie sich über Typen in C++. 
 Benennen Sie im Folgenden Beispiel Typen, Variablen und Werte. 
 Erklären Sie in diesem Zusammenhang das Schlüs- selwort const. 
@@ -26,4 +27,19 @@ int main() {
 // Typen: 		int, bool, char, double 
 // Variablen: 	a,b,c,d,c,five, e
 // Werte:		9, false, 'a', 1.3, 5, a/five , d, 0;
+
+//Const 
+//- means that the assigned value will not be allowed to change during the running of the programm, 
+// it remains as a fixed Constant throughout the lifetime of the programm during the runtime.
+// trying to change the assigned value in the programm after assigning a value to it will result in a runtime error
+// as ccan be expected with the part where (five = d)
+
+//Typkonvertierung
+//This is the conversion on one Primary data type to the other. For example from an INTEGER to a DOUBLE 
+//This results in  compatibility issues and can cause data loss especially when converting from a bigger memory space into a smaller one
+// for example the type conversion of a DOUBLE to INTERGER results in the loss of all data after the COMMA. 
+
+//Problems in the programm
+//1) The reassignemnt of a const value (five = d) will result in an error
+//2) The type conversion of the double into an int value will result in data loss
 
