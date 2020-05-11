735abc585c7969d53afcbbd10b85230fd5276938
Philbranding <philbranding@gmail.com>
2020-05-11 08:24:17 +0200

    done with Assignment 1.3

---
+++ build/source/CMakeFiles/aufgabe1_3.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/phil/Git/Programmiersprache/01/programmiersprachen-aufgabe-1/source/aufgabe1_3.cpp" "/Users/phil/Git/Programmiersprache/01/programmiersprachen-aufgabe-1/build/source/CMakeFiles/aufgabe1_3.dir/aufgabe1_3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/catch-1.4.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/source/CMakeFiles/aufgabe1_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe1_3.dir/aufgabe1_3.cpp.o"
+  "aufgabe1_3"
+  "aufgabe1_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufgabe1_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
